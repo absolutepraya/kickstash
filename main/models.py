a31c390242aae90333c293859231c18c4d21feec
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-09-06 15:10:51 +0700

    Main views done

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=15)
+    description = models.TextField()
+
+    @property
+    def price_display(self):
+        return f'Rp {self.price}'
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Birkenstock Boston Taupe Suede',
+        'price': 2599000,
+        'description': 'Birkenstock Boston Taupe Suede adalah sandal yang nyaman digunakan sehari-hari. Dengan bahan kulit suede yang lembut dan nyaman saat digunakan.'
+    }
+
+    return render(request, 'main.html', context)
