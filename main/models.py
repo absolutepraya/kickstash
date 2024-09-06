a32d929c20c8c4149e5f5587949ff9f54aa91d8b
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-09-06 22:42:40 +0700

    Overall update

--- main/models.py
+++ main/models.py
@@ -4,7 +4,3 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=2, max_digits=15)
     description = models.TextField()
-
-    @property
-    def price_display(self):
-        return f'Rp {self.price}'
--- main/views.py
+++ main/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from .models import Product
 
 def show_main(request):
+    # Get the first product
+    # product = Product.objects.first()
+
+    # Prepare context
+    # context = {
+    #     'name': product.name,
+    #     'price': product.price,
+    #     'description': product.description,
+    # }
+
+    # Prepare pre-made context
     context = {
-        'name': 'Birkenstock Boston Taupe Suede',
+        'name': 'Birkenstock Boston',
         'price': 2599000,
-        'description': 'Birkenstock Boston Taupe Suede adalah sandal yang nyaman digunakan sehari-hari. Dengan bahan kulit suede yang lembut dan nyaman saat digunakan.'
+        'description': 'Birkenstock Boston adalah sandal yang nyaman digunakan untuk berbagai aktivitas.'
     }
 
     return render(request, 'main.html', context)
