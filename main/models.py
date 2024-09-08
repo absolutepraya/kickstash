2fdb68223dc551716a9276109c9b8de39d434db1
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-09-08 22:09:15 +0700

    Models update & added test units

--- main/models.py
+++ main/models.py
@@ -4,3 +4,8 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=2, max_digits=15)
     description = models.TextField()
+    stock = models.IntegerField(default=1)
+
+    @property
+    def is_available(self):
+        return self.stock > 0
