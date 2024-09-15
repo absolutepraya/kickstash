5d363e7f966418b583e853ab290a3a2dfa0d12e5
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-09-15 23:29:23 +0700

    Tugas 3 (code) done

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+import uuid
+
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=2, max_digits=15)
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,27 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
 from .models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
-def show_main(request):
-    # Get the first product
-    # product = Product.objects.first()
 
-    # Prepare context
-    # context = {
-    #     'name': product.name,
-    #     'price': product.price,
-    #     'description': product.description,
-    # }
+def show_main(req):
+    products = Product.objects.all()
 
-    # Prepare pre-made context
     context = {
-        'name': 'Birkenstock Boston',
-        'price': 2599000,
-        'description': 'Birkenstock Boston adalah sandal yang nyaman digunakan untuk berbagai aktivitas.',
-
         # Credentials
-        'student_name': 'Daffa Abhipraya Putra',
-        'student_id': '2306245131',
-        'student_class': 'Kelas PBP D'
+        "student_name": "Daffa Abhipraya Putra",
+        "student_id": "2306245131",
+        "student_class": "Kelas PBP D",
+        # Data
+        "products": products,
     }
 
-    return render(request, 'main.html', context)
+    return render(req, "main.html", context)
+
+
+def create_product(req):
+    form = ProductForm(req.POST or None)
+
+    if form.is_valid() and req.method == "POST":
+        form.save()
+        return redirect("main:show_main")
+
+    context = {"form": form}
+    return render(req, "create_product.html", context)
+
+
+def show_xml(req):
+    data = Product.objects.all()
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
+
+
+def show_json(req):
+    data = Product.objects.all()
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
+
+
+def show_xml_by_id(req, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
+
+
+def show_json_by_id(req, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
