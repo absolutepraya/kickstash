979ff60636d1b086409e71219b6935b91034de68
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-09-15 23:43:07 +0700

    Migrated model change

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ import uuid
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.DecimalField(decimal_places=2, max_digits=15)
+    price = models.DecimalField(decimal_places=0, max_digits=15)
     description = models.TextField()
     stock = models.IntegerField(default=1)
 
