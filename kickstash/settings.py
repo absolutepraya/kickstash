2a53851e282c238d6db81771fdb05046be073eb3
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-09-23 12:13:52 +0700

    Done Tugas 4, made small stylings to make overall HTML looks slick

--- kickstash/settings.py
+++ kickstash/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(2(8gwhi#9kb2rfq(ud^xw@9bcydwgm4eq+$w&t+7zoapq1%t$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = [
     "localhost",
