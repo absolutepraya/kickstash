0c9ca2ca14cecce8620e44feef30e60c969ce6ce
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-10-08 19:45:07 +0700

    Why did I even change the debug to True...

--- kickstash/settings.py
+++ kickstash/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(2(8gwhi#9kb2rfq(ud^xw@9bcydwgm4eq+$w&t+7zoapq1%t$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = [
     "localhost",
